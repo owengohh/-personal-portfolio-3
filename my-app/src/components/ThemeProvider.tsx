1bb95ab40bc24253d4522b443480f3882d1f1ac3
Owen Goh <owengoh2001@gmail.com>
2024-07-17 10:41:23 +0800

    fix bugs

--- my-app/src/components/ThemeProvider.tsx
+++ my-app/src/components/ThemeProvider.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, ...props }) {
+import { ReactNode } from "react";
+
+export function ThemeProvider({ children, ...props }: { children: ReactNode, [key: string]: any }) {
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
